f4b67c07dc5af7b8e18895b8a0d97b42ce131d22
thulas42 <73069032+thulas42@users.noreply.github.com>
2025-03-01 22:34:06 +0200

    Add project files.

---
+++ tumishoStudyGroup/Controllers/StudentController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace tumishoStudyGroup.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Students()
+        {
+            List<Models.StudentModel> students = new List<Models.StudentModel>();
+            students.Add(new Models.StudentModel { StudentNumber = "u24885062", StudentName = "Tumisho", StudentSurname = "Matlala", StudentEmail = "tumisho.matlala@tuks.co.za" });
+            students.Add(new Models.StudentModel { StudentNumber = "u22746995", StudentName = "Kgalalelo", StudentSurname = "Lethoko", StudentEmail = "kgalalelo.lethoko@tuks.co.za" });
+            students.Add(new Models.StudentModel { StudentNumber = "u20731028", StudentName = "Nomvuyo", StudentSurname = "Mthimkhulu", StudentEmail = "nomvuyo.mthimkhulu@tuks.co.za" });
+            students.Add(new Models.StudentModel { StudentNumber = "u24753328", StudentName = "Bulelo", StudentSurname = "Sibisi", StudentEmail = "bulelo.sibisi@tuks.co.za" });
+            students.Add(new Models.StudentModel { StudentNumber = "u24986242", StudentName = "Palesa", StudentSurname = "Mphephu", StudentEmail = "palesa.mphephu@tuks.co.za" });
+
+
+            return View(students);
+        }
+    }
+}
---
+++ tumishoStudyGroup/Models/StudentModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace tumishoStudyGroup.Models
+{
+    public class StudentModel
+    {
+        [Display(Name = "STUDENT NUMBER")]
+        public string StudentNumber { get; set; }
+
+        [Display(Name = "NAME")]
+        public string StudentName { get; set; }
+
+        [Display(Name = "SURNAME")]
+        public string StudentSurname { get; set; }
+
+        [Display(Name = "EMAIL ADDRESS")]
+        public string StudentEmail { get; set; }
+    }
+}
